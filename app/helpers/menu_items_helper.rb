c1123b0b9337b42ea8e7bde4fb016f3140b6a78f
JuliKroiter <kroiter09@gmail.com>
2022-06-19 20:55:37 +0300

    Add menu ul li styles

--- app/helpers/menu_items_helper.rb
+++ app/helpers/menu_items_helper.rb
@@ -3,12 +3,12 @@ module MenuItemsHelper
     li = String.new
 
     roots.each do |root|
-      li << content_tag(:li, root.name)
+      li << content_tag(:li, root.name, class: "menu_li #{root.state}")
       if root.has_child?
         li << menu_list(root.child_menu_items)
       end
     end
 
-    content_tag :ul, li.html_safe
+    content_tag :ul, li.html_safe, class: 'menu_ul'
   end
 end
